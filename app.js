93a4a02cec11aedbf9180c8c5fda3b54dac656b2
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-30 23:02:12 +0530

    added cors issue fix

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 
+app.use((req, res, next) => {
+  // Set which domains can send requests
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Set types of HTTP requests to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  // Set the types of headers that the client can set on their requests
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // ROUTES
 app.use("/feed", feedRoutes);
 
