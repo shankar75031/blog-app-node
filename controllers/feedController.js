260c69a440bdaa3998d9cbba0d9c37a98f548c26
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-31 00:19:17 +0530

    added validation for post creation title and content

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,19 +1,45 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: "1",
         title: "First post",
         content: "THis is a sample content of first post",
+        imageUrl: "images/duck.jpg",
+        creator: {
+          name: "Kunj",
+        },
+        createdAt: new Date(),
       },
     ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation failed, entered data is incorrect",
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
     message: "Post created successfully",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+      imageUrl: "images/duck.jpg",
+      creator: {
+        name: "Kunj",
+      },
+      createdAt: new Date(),
+    },
   });
 };
