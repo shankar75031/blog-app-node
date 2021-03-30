452d6f4185247f2c7ce244720b0e38520f5c83da
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-31 00:38:53 +0530

    saved post to db

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -20,26 +21,29 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect",
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  res.status(201).json({
-    message: "Post created successfully",
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
-      imageUrl: "images/duck.jpg",
-      creator: {
-        name: "Kunj",
-      },
-      createdAt: new Date(),
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/duck.jpg",
+    creator: {
+      name: "Kunj",
     },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result,
+      });
+    })
+    .catch((err) => console.log(err));
 };
