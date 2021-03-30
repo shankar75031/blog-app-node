09e3259c37a6ce08d3969f1e4cc5a8e99de4fc6a
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-31 03:18:33 +0530

    added jwt token validation middleware

--- routes/feed.js
+++ routes/feed.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const { body } = require("express-validator");
+const isAuth = require("../middleware/is-auth");
 const feedController = require("../controllers/feedController");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 router.post(
   "/post",
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   feedController.createPost
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 router.put(
   "/post/:postId",
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   feedController.updatePost
 );
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
