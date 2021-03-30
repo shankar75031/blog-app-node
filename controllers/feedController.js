45bf8bbc5043b9b801297571bcb7cf67fc169fb4
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-31 03:48:31 +0530

    async await added in get posts api

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -5,28 +5,22 @@ const Post = require("../models/post");
 const User = require("../models/user");
 const { handleError } = require("../utils/errorUtils");
 
-exports.getPosts = (req, res, next) => {
-  const currentPage = req.query.page || 1;
-  const perPage = 2;
-  let totalItems;
-  Post.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((posts) => {
-      res.status(200).json({
-        message: "Fetched posts successfully",
-        posts: posts,
-        totalItems: totalItems,
-      });
-    })
-    .catch((err) => {
-      handleError(err, next);
+exports.getPosts = async (req, res, next) => {
+  try {
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    const totalItems = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+    res.status(200).json({
+      message: "Fetched posts successfully",
+      posts: posts,
+      totalItems: totalItems,
     });
+  } catch (error) {
+    handleError(error, next);
+  }
 };
 
 exports.createPost = (req, res, next) => {
